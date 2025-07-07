21da913e87a07e850ebc177a39f7e26918c12957
Aaron Hutchinson <111082932+L3vinax@users.noreply.github.com>
2025-07-07 14:46:49 -0700

    Create updateAMA.ps1
    initial upload

---
+++ updateAMA.ps1
@@ -0,0 +1,19 @@
+$rgName = "<RG NAME>"
+$region = "<Region of Arc objects>"
+$amaName = "AzureMonitorWindowsAgent"
+$publisher = "Microsoft.Azure.Monitor"
+$settings = @{}
+
+$latest = (Get-AzVMExtensionImage -Location $region -PublisherName $publisher -Type $amaName | Sort-Object { [Version]$_.Version } -Descending | Select-Object -First 1).Version
+$servers = Get-AzConnectedMachine -ResourceGroupName $rgName
+
+$servers | ForEach-Object -Parallel {
+    $ext = Get-AzConnectedMachineExtension -ResourceGroupName $using:rgName -MachineName $_.Name -Name $using:amaName -ErrorAction SilentlyContinue
+    $currentVersion = if ($ext) { $ext.TypeHandlerVersion } else { "0.0.0.0" }
+    if ([Version]$currentVersion -lt [Version]$using:latest) {
+        New-AzConnectedMachineExtension -Name $using:amaName -ExtensionType $using:amaName -Publisher $using:publisher -ResourceGroupName $using:rgName -MachineName $_.Name -Location $using:region -TypeHandlerVersion $using:latest -Settings $using:settings | Out-Null
+        Write-Host "Updated AMA on $($_.Name) from $currentVersion to $using:latest" -ForegroundColor Green
+    } else {
+        Write-Host "AMA on $($_.Name) is already up to date ($currentVersion)" -ForegroundColor Yellow
+    }
+}
